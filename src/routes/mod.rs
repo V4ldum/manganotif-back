28a4ccf453cbe013030d8487ac14fdffd8639ac1
V4ldum <56064897+V4ldum@users.noreply.github.com>
2025-11-12 19:41:20 +0100

    mangas behind auth + adm endpoint

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -18,11 +18,11 @@ pub(super) fn router(state: AppState) -> axum::Router {
     //    .allow_origin(Any);
 
     Router::new()
-        .route("/manga/not_found", get(get_all_mangas_not_found))
-        .route("/manga/global", get(get_all_global_mangas))
+        .route("/adm/manga", get(get_all_mangas))
+        .route("/adm/manga/not_found", get(get_all_mangas_not_found))
+        .route("/adm/manga/global", get(get_all_global_mangas))
         // Anything above needs authentication
         .route_layer(from_fn_with_state(state.clone(), check_auth))
-        .route("/manga", get(get_all_mangas))
         // Anything above can use the state
         .with_state(state)
         .route("/health", get(health_check))
